7fa6cd064a0c5ae2fbfdc38523b7b16704886fef
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2023-05-30 16:27:52 -0400

    set up new ui

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let SIZE = 16;
+
 const grid = document.querySelector('#grid')
-for(i=0; i<16; i++){
+for(i=0; i<SIZE; i++){
     let row = document.createElement('div')
     row.classList.add('row')
     grid.appendChild(row)
@@ -7,7 +9,7 @@ for(i=0; i<16; i++){
 
 const rows = document.querySelectorAll('#grid .row')
 rows.forEach( (x)=>{
-    for(i=0; i<16; i++){
+    for(i=0; i<SIZE; i++){
         let box = document.createElement('div')
         box.classList.add('box')
         x.appendChild(box)
